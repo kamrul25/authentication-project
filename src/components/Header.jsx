2d93ce7ac1ab4221f9398f4c2e42d2685631dbe1
kamrul25 <kamrul.coder01@gmail.com>
2023-05-03 14:28:53 +0600

    send email and password to firebase successfully

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/signIn">Sign In</Link>
-            <Link to="/signUp">Sign Up</Link>
+        <nav className="text-center text-3xl font-semibold mt-8">
+            <Link to="/" className="mr-4">Home</Link>
+            <Link to="/signIn" className="mr-4">Sign In</Link>
+            <Link to="/signUp" className="mr-4">Sign Up</Link>
             
         </nav>
     );
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
+const auth = getAuth(app);
 const Register = () => {
-    return (
-        <div>
-            <h2>From Register Page</h2>
-        </div>
-    );
+  const [email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    //    1. prevent page refresh
+    event.preventDefault();
+    // 2. collect form data
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+    // 3. create user in firebase
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const registered = result.user;
+        console.log(registered);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const handleEmailChange = (event) => {
+    //   console.log(event.target.value)
+    const value = event.target.value;
+    setEmail(value);
+  };
+  const handleChangedPassword = (event) => {
+    const value = event.target.value;
+    // console.log(value)
+    setPassword(value);
+  };
+  return (
+    <div className="text-center">
+      <h2 className="text-2xl font-medium mx-4 mt-4">Please Register </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleEmailChange}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Your Email"
+          className="text-2xl font-medium m-4 border rounded"
+          required
+        />
+        <br />
+        <input
+          onBlur={handleChangedPassword}
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Your Password"
+          className="text-2xl font-medium m-4 border rounded "
+          required
+        />
+        <br />
+        <input
+          type="submit"
+          value="Register"
+          className="text-2xl font-medium m-4  border rounded-lg px-4 py-2"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Register;
